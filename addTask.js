d9000f73092cb05407637fade3dde66e7bc23abb
Kummey <76731996+Kummey@users.noreply.github.com>
2021-01-02 11:52:44 +0100

    update addTask.js

--- addTask.js
+++ addTask.js
@@ -1,15 +1,61 @@
-let users = [
-    ["Alexander Kummerer", "alexander@kummerer.mail", "img/person-24px.svg"],
-    ["Dan Mercurean", "dan@mercurean.mail", "img/person-24px.svg"],
-    ["Eileen Moore", "eileen@moore.mail", "img/person-24px.svg"],
-    ["Jaci jack", "jaci@jack.mail", "img/person-24px.svg"],
-    ["Junus Ergin", "junus@Ergin.mail", "img/person-24px.svg"],
-    ["Manuel Thaler", "manuel@thaler.mail", "img/person-24px.svg"],
+//Json of person where can selected
+let persons = [{
+        name: "Alexander Kummerer",
+        mail: "alexander@kummerer.mail",
+        img: "img/nutzer.svg",
+    },
+    {
+        name: "Eileen Moore",
+        mail: "eileen@moore.mail",
+        img: "img/nutzer.svg",
+    },
+    {
+        name: "Dan Mercurean",
+        mail: "dan@mercurean.mail",
+        img: "img/nutzer.svg",
+    },
+    {
+        name: "Jaci jack",
+        mail: "jaci@jack.maill",
+        img: "img/nutzer.svg",
+    },
+    {
+        name: "Junus Ergin",
+        mail: "junus@ergin.mail",
+        img: "img/nutzer.svg",
+    },
+    {
+        name: "Manuel Thaler",
+        mail: "manuel@thaler.maill",
+        img: "img/nutzer.svg",
+    },
 ];
 
-function addPerson() {
+// personBlen with users to selct will displayed
+
+function addPersonBlend() {
     document.getElementById("addPersonBlend").classList.remove("d-none");
 }
+
+
+
+
+function displayperson() {
+    for (let i = 0; i < persons.length; i++) {
+        const person = persons[i];
+        document.getElementById("user-picker-container").innerHTML += `
+        <div id="user-picker-row${i}" class ="user-picker-row" onclick="selectUser(${i})"> <img src="${person["img"]}"> <span>${person["name"]} </span> </div>
+    `;
+    }
+}
+
+function selectUser(person) {
+    document.getElementById("user-picker-row{i}").classList.toggle("user-picker-row-select");
+
+}
+
+
+//personBlend will removed
 
 function removePersonBlend() {
     document.getElementById("addPersonBlend").classList.add("d-none");
@@ -17,5 +63,4 @@ function removePersonBlend() {
 
 function createTask($event) {
     $event.preventDefault();
-
 }
