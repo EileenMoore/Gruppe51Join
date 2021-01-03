aa4202b43412b9fa975252d29f5682996922785e
Kummey <76731996+Kummey@users.noreply.github.com>
2021-01-03 16:19:12 +0100

    update  unction selectUser(i)

--- addTask.js
+++ addTask.js
@@ -1,34 +1,35 @@
 //Json of person where can selected
 let persons = [{
-    name: "Alexander Kummerer",
-    mail: "alexander@kummerer.mail",
-    img: "img/alex.jpg"
-},
-{
-    name: "Eileen Moore",
-    mail: "eileen@moore.mail",
-    img: "img/eileen.jpg"
-},
-{
-    name: "Dan Mercurean",
-    mail: "dan@mercurean.mail",
-    img: "img/person-24px.svg"
-},
-{
-    name: "Jaci jack",
-    mail: "jaci@jack.maill",
-    img: "img/nutzer.svg"
-},
-{
-    name: "Junus Ergin",
-    mail: "junus@ergin.mail",
-    img: "img/junus.jpg"
-},
-{
-    name: "Manuel Thaler",
-    mail: "manuel@thaler.maill",
-    img: "img/manuel.jpg"
-}];
+        name: "Alexander Kummerer",
+        mail: "alexander@kummerer.mail",
+        img: "img/alex.jpg",
+    },
+    {
+        name: "Eileen Moore",
+        mail: "eileen@moore.mail",
+        img: "img/eileen.jpg",
+    },
+    {
+        name: "Dan Mercurean",
+        mail: "dan@mercurean.mail",
+        img: "img/person-24px.svg",
+    },
+    {
+        name: "Jaci jack",
+        mail: "jaci@jack.maill",
+        img: "img/nutzer.svg",
+    },
+    {
+        name: "Junus Ergin",
+        mail: "junus@ergin.mail",
+        img: "img/junus.jpg",
+    },
+    {
+        name: "Manuel Thaler",
+        mail: "manuel@thaler.maill",
+        img: "img/manuel.jpg",
+    },
+];
 
 let selectedUsers = [];
 
@@ -48,16 +49,17 @@ function displayperson() {
 
 function selectUser(i) {
     let id = "user-picker-row" + i;
-    let userImage = persons[i]["img"];
+    let userImage = persons[i];
     let imagefound = false;
 
     for (let j = 0; j < selectedUsers.length; j++) {
-        if (selectedUsers[j] == persons[i].img) {
+        if (selectedUsers[j]["img"] == persons[i]["img"]) {
             imagefound = true;
             document.getElementById(id).classList.remove("user-picker-row-select");
             selectedUsers.splice(j, 1);
         }
-    } if (!imagefound) {
+    }
+    if (!imagefound) {
         document.getElementById(id).classList.add("user-picker-row-select");
         selectedUsers.push(userImage);
     }
@@ -66,25 +68,19 @@ function selectUser(i) {
 }
 
 function displaySelectedUsers() {
-    document.getElementById("assign-person").innerHTML = '';
+    document.getElementById("assign-person").innerHTML = "";
     for (let j = 0; j < selectedUsers.length; j++) {
         document.getElementById("assign-person").innerHTML += `
-        <img src="${selectedUsers[j]}">`;
+        <img src="${selectedUsers[j]["img"]}">`;
     }
 }
 
 function removePerson() {
     document.getElementById("assign-person").innerHTML = "";
-    if (document.getElementById("assign-person").contains.innerHTML = `
-    <img src="${persons[i]["img"]}"> `) {
-        document.getElementById("assign-person").remove.innerHTML = `<img src="${persons[i]["img"]}">`;
-    }
-
+    selectedUsers = [];
 }
 
 //personBlend will removed
-
-
 
 function removePersonBlend() {
     document.getElementById("addPersonBlend").classList.add("d-none");
@@ -93,4 +89,3 @@ function removePersonBlend() {
 function createTask($event) {
     $event.preventDefault();
 }
-
