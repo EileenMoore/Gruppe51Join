e6cd6823f96a2f4a0bcdb7972a54587d2b067061
unknown <eileen-moore@hotmail.de>
2021-01-03 17:56:18 +0100

    remove function

--- addTask.js
+++ addTask.js
@@ -1,33 +1,33 @@
 //Json of person where can selected
 let persons = [{
-    name: "Alexander Kummerer",
-    mail: "alexander@kummerer.mail",
-    img: "img/alex.jpg"
+    'name': "Alexander Kummerer",
+    'mail': "alexander@kummerer.mail",
+    'img': "img/alex.jpg"
 },
 {
-    name: "Eileen Moore",
-    mail: "eileen@moore.mail",
-    img: "img/eileen.jpg"
+    'name': "Eileen Moore",
+    'mail': "eileen@moore.mail",
+    'img': "img/eileen.jpg"
 },
 {
-    name: "Dan Mercurean",
-    mail: "dan@mercurean.mail",
-    img: "img/person-24px.svg"
+    'name': "Dan Mercurean",
+    'mail': "dan@mercurean.mail",
+    'img': "img/person-24px.svg"
 },
 {
-    name: "Jaci jack",
-    mail: "jaci@jack.maill",
-    img: "img/nutzer.svg"
+    'name': "Jaci jack",
+    'mail': "jaci@jack.maill",
+    'img': "img/nutzer.svg"
 },
 {
-    name: "Junus Ergin",
-    mail: "junus@ergin.mail",
-    img: "img/junus.jpg"
+    'name': "Junus Ergin",
+    'mail': "junus@ergin.mail",
+    'img': "img/junus.jpg"
 },
 {
-    name: "Manuel Thaler",
-    mail: "manuel@thaler.maill",
-    img: "img/manuel.jpg"
+    'name': "Manuel Thaler",
+    'mail': "manuel@thaler.maill",
+    'img': "img/manuel.jpg"
 }];
 
 let selectedUsers = [];
@@ -48,38 +48,46 @@ function displayperson() {
 
 function selectUser(i) {
     let id = "user-picker-row" + i;
-    let userImage = persons[i]["img"];
-    let imagefound = false;
+    let user = {
+        'username': persons[i]["name"],
+        'userImage': persons[i]["img"]
+    }
 
+    checkIfUserIsAlreadySelected(i, id, user);
+    console.log(selectedUsers);
+    displaySelectedUsers();
+}
+
+function checkIfUserIsAlreadySelected(i, id, user){
+    let userfound  = false;
     for (let j = 0; j < selectedUsers.length; j++) {
-        if (selectedUsers[j] == persons[i].img) {
-            imagefound = true;
+        if (selectedUsers[j].username == persons[i].name) {
+            userfound = true;
             document.getElementById(id).classList.remove("user-picker-row-select");
             selectedUsers.splice(j, 1);
         }
-    } if (!imagefound) {
+    } if (!userfound) {
         document.getElementById(id).classList.add("user-picker-row-select");
-        selectedUsers.push(userImage);
+        selectedUsers.push(user);
     }
-    console.log(selectedUsers);
-    displaySelectedUsers();
 }
 
 function displaySelectedUsers() {
     document.getElementById("assign-person").innerHTML = '';
     for (let j = 0; j < selectedUsers.length; j++) {
         document.getElementById("assign-person").innerHTML += `
-        <img src="${selectedUsers[j]}">`;
+        <img src="${selectedUsers[j]['userImage']}">`;
     }
 }
 
 function removePerson() {
-    document.getElementById("assign-person").innerHTML = "";
-    if (document.getElementById("assign-person").contains.innerHTML = `
-    <img src="${persons[i]["img"]}"> `) {
-        document.getElementById("assign-person").remove.innerHTML = `<img src="${persons[i]["img"]}">`;
-    }
+    selectedUsers = [];
 
+    for (let i = 0; i < persons.length; i++) {
+        let id = "user-picker-row" + i;
+        document.getElementById(id).classList.remove("user-picker-row-select");
+    }
+    displaySelectedUsers();
 }
 
 //personBlend will removed
