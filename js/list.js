537a02fdb63ed94019f3d9080012ad23fb987344
Dan Mercurean <danymercurean@yahoo.com>
2021-01-04 21:34:25 +0100

    list.js update

--- js/list.js
+++ js/list.js
@@ -1,12 +1,14 @@
-function generateListItem(persons, task) {
-    return `
+
+function generateListItem() {
+    for (i = 0; i < persons.length; i++) {
+        document.getElementById('taskdelegate').innerHTML +=`
     <div class="task-item-content">
     <div class="assigned-content">
-        <img src="${persons.img}" class="assigned-img rounded-circle">
-        <div>${persons.name}<br>${user.email}</div>
+        <img src="${persons[i].img}" class="assigned-img rounded-circle">
+        <div>${persons[i].name}<br>${persons[i].mail}</div>
     </div>
-    <div class="category-content">${task.category}</div>
-    <div class="details-content">${task.description}</div>
+    <div class="category-content">Sales</div>
     </div>
 `;
+    }
 }
