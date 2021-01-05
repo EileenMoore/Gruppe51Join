743b2ac14dec16c738c6272e5e7d9ad54b9801cb
Dan Mercurean <danymercurean@yahoo.com>
2021-01-05 17:29:47 +0100

    update list.js

--- js/list.js
+++ js/list.js
@@ -1,16 +1,16 @@
 function generateListItem() {
     loadAllTasks();
     for (i = 0; i < allTasks.length; i++) {
-        for (j = 0; j < allTasks[i].assignedPerson.length; j++){
+        for (j = 0; j < allTasks[i].assignedPeople.length; j++){
+            let assignedPeople = allTasks[j].assignedPeople[j];
         document.getElementById('taskdelegate').innerHTML += `
     <div class="task-item-content">
     <div class="assigned-content">
-        <img src="${allTasks[i]['assignedPerson'][j]['profilePicture']}" class="assigned-img rounded-circle">
-        <div>${allTasks[i].username}<br>${allTasks[i].email}</div>
+        <img src="${allTasks[i]['assignedPeople'][j]['profilePicture']}" class="assigned-img rounded-circle">
+        <div>${assignedPeople.username}<br>${assignedPeople.email}</div>
     </div>
     <div class="category-content">${allTasks[i]['category']}</div>
-    <div class="category-content">${allTasks[i]['discription']}</div>
-    </div>
-`;
+    <div class="category-content">${allTasks[i]['description']}</div>
+    </div>`;
         }  }
 }
