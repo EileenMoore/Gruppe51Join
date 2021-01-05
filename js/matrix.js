2b6cbee4e2b3951aab424c8c7590634d5e73d13b
unknown <eileen-moore@hotmail.de>
2021-01-05 16:13:24 +0100

    sort tasks in matrix

--- js/matrix.js
+++ js/matrix.js
@@ -1,34 +1,95 @@
+let doTasks = [];
+let scheduleTasks = [];
+let delegateTasks = [];
+let eliminateTasks = [];
 
-function loadCurrentUser() {
-    let currentUserAsString = localStorage.getItem('currentUser');
+function sortTasks() {
+    loadAllTasks();
 
-    if (currentUserAsString) {
-        currentUser = JSON.parse(currentUserAsString);
-        console.log(currentUser);
+    for (let i = 0; i < allTasks.length; i++) {
+        if (allTasks[i].section == 'Do')
+            doTasks.push(allTasks[i]);
+
+        if (allTasks[i].section == 'Schedule')
+            scheduleTasks.push(allTasks[i]);
+
+        if (allTasks[i].section == 'Delegate')
+            delegateTasks.push(allTasks[i]);
+
+        if (allTasks[i].section == 'Eliminate')
+            eliminateTasks.push(allTasks[i]);
     }
 }
 
 function insertTasks() {
+    sortTasks();
+    let section;
 
-    loadAllTasks();
-    loadAllUsers();
-    loadCurrentUser();
-    console.log(allTasks);
-    console.log(users);
+    insertDoTasks(section) ;
+    insertScheduleTasks(section);
+    insertDelegateTasks(section);
+    insertEliminateTasks(section);
+}
 
-    for (i = 0; i < allTasks.length; i++) {
-        document.getElementById('do').innerHTML += `
-    <div class="task-card do">
-    <span class="date">${allTasks[i]['date']}</span>
-    <span class="task-card-headline">${allTasks[i]['title']}</span>
-    <span class="task-card-description">${allTasks[i]['discription']}</span>
-    <div class="task-card-bottom">
-        <button type="button" class="btn btn-primary">${allTasks[i]['category']}</button>
-        <div class="profile-picture-small"> <img class="profile-picture-small" src="${allTasks[i]['assignedPerson'][0]['img']}"></div>
-        <img id="myImage" src"">
-    </div>
-    </div>
-    `;
+function insertDoTasks(section) {
+    for (i = 0; i < doTasks.length; i++) {
+        const task = doTasks[i];
+        section = 'do';
+        document.getElementById('do').innerHTML +=
+            generateTask(task, section);
     }
+}
+
+function insertScheduleTasks(section) {
+    for (i = 0; i < scheduleTasks.length; i++) {
+        const task = scheduleTasks[i];
+        section = 'schedule';
+        document.getElementById('schedule').innerHTML +=
+            generateTask(task, section);
+    }
+}
+
+function insertDelegateTasks(section) {
+    for (i = 0; i < delegateTasks.length; i++) {
+        const task = delegateTasks[i];
+        section = 'delegate';
+        document.getElementById('delegate').innerHTML +=
+            generateTask(task, section);
+    }
+}
+
+function insertEliminateTasks(section) {
+    for (i = 0; i < eliminateTasks.length; i++) {
+        const task = eliminateTasks[i];
+        section = 'eliminate';
+        document.getElementById('eliminate').innerHTML +=
+            generateTask(task, section);
+    }
+}
+
+function generateTask(task, section) {
+    return `
+    <div class="task-card ${section}">
+    <div class="task-card-left">
+        <div>
+            <div class="date">${task['date']}</div>
+            <div class="task-card-headline">${task['title']}</div>
+            <div class="task-card-description">${task['description']}</div>
+        </div>
+    <button type="button" class="btn btn-primary">${task['category']}</button>
+    </div>
+    ${generateImageRow(task)}
+</div>`
+}
+
+function generateImageRow(task) {
+    let imgRow = `<div class="profile-pictures">`;
+
+    for (j = 0; j < task['assignedPeople'].length; j++) {
+        imgRow += `<img class="profile-picture-small" src="${task['assignedPeople'][j]['profilePicture']}">`
+    }
+
+    imgRow += `</div>`;
+    return imgRow;
 }
 
