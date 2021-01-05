36be53467131e83db47abb35382cb5e61bca2e52
Dan Mercurean <danymercurean@yahoo.com>
2021-01-05 21:19:35 +0100

    update

--- js/list.js
+++ js/list.js
@@ -1,10 +1,14 @@
+/**
+ * This function shows the tasks in the list
+ */
+
 function generateListItem() {
     loadAllTasks();
     for (i = 0; i < allTasks.length; i++) {
         for (j = 0; j < allTasks[i].assignedPeople.length; j++){
-            let assignedPeople = allTasks[j].assignedPeople[j];
+            let assignedPeople = allTasks[i].assignedPeople[j];
         document.getElementById('taskdelegate').innerHTML += `
-    <div class="task-item-content">
+    <div class="task-item-content ${allTasks[i].section}">
     <div class="assigned-content">
         <img src="${allTasks[i]['assignedPeople'][j]['profilePicture']}" class="assigned-img rounded-circle">
         <div>${assignedPeople.username}<br>${assignedPeople.email}</div>
