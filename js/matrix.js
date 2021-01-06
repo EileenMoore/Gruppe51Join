948e9b5d647c62f0c84c98678d106a92fc1ccfe2
unknown <eileen-moore@hotmail.de>
2021-01-06 16:13:27 +0100

    matrix update

--- js/matrix.js
+++ js/matrix.js
@@ -4,98 +4,87 @@ let delegateTasks = [];
 let eliminateTasks = [];
 
 /**
- * This function sorts all Tasks in sub sections.
+ * This function sorts the entries of the allTasks array into subsections.
  */
 function sortTasks() {
     loadAllTasks();
 
     for (let i = 0; i < allTasks.length; i++) {
-        if (allTasks[i].section == 'Do')
-            doTasks.push(allTasks[i]);
-
-        if (allTasks[i].section == 'Schedule')
-            scheduleTasks.push(allTasks[i]);
-
-        if (allTasks[i].section == 'Delegate')
-            delegateTasks.push(allTasks[i]);
-
-        if (allTasks[i].section == 'Eliminate')
-            eliminateTasks.push(allTasks[i]);
+        sortDoTasks(i);
+        sortScheduleTasks(i);
+        sortDelegateTasks(i);
+        sortEliminateTasks(i);
     }
 }
 
 /**
- * This function inserts the taks from the sub sections into the matrix fields.
- */
-function insertTasks() {
-    sortTasks();
-    let section;
-
-    insertDoTasks(section) ;
-    insertScheduleTasks(section);
-    insertDelegateTasks(section);
-    insertEliminateTasks(section);
-}
-
-/**
- * This function inserts the tasks from the do-section into the do-field in the matrix.
+ * This function sorts the tasks with do-section into the doTasks array.
  * 
  * 
- * @param {string} section - This string represents a task-section.
+ * @param {number} i - This represents the number of all tasks.
  */
-function insertDoTasks(section) {
-    for (i = 0; i < doTasks.length; i++) {
-        const task = doTasks[i];
-        section = 'do';
-        document.getElementById('do').innerHTML +=
-            generateTask(task, section);
+function sortDoTasks(i) {
+    if (allTasks[i].section == 'do') {
+        doTasks.push(allTasks[i]);
+        let subTasks = doTasks;
+        insertTasks(subTasks);
     }
 }
 
 /**
- * This function inserts the tasks from the schedule-section into the schedule-field in the matrix.
+ * This function sorts the tasks with schedule-section into the scheduleTasks array.
  * 
  * 
- * @param {string} section - This string represents a task-section.
+ * @param {number} i - This represents the number of all tasks.
  */
-
-function insertScheduleTasks(section) {
-    for (i = 0; i < scheduleTasks.length; i++) {
-        const task = scheduleTasks[i];
-        section = 'schedule';
-        document.getElementById('schedule').innerHTML +=
-            generateTask(task, section);
+function sortScheduleTasks(i) {
+    if (allTasks[i].section == 'schedule') {
+        scheduleTasks.push(allTasks[i]);
+        let subTasks = scheduleTasks;
+        insertTasks(subTasks);
     }
 }
 
 /**
- * This function inserts the tasks from the delegate-section into the delegate-field in the matrix.
+ * This function sorts the tasks with delegate-section into the delegateTasks array.
  * 
  * 
- * @param {string} section - This string represents a task-section.
+ * @param {number} i - This represents the number of all tasks.
  */
-function insertDelegateTasks(section) {
-    for (i = 0; i < delegateTasks.length; i++) {
-        const task = delegateTasks[i];
-        section = 'delegate';
-        document.getElementById('delegate').innerHTML +=
-            generateTask(task, section);
+function sortDelegateTasks(i) {
+    if (allTasks[i].section == 'delegate') {
+        delegateTasks.push(allTasks[i]);
+        let subTasks = delegateTasks;
+        insertTasks(subTasks);
     }
 }
 
 /**
- * This function inserts the tasks from the eliminate-section into the eliminate-field in the matrix.
+ * This function sorts the tasks with eliminate-section into the eliminateTasks array.
  * 
  * 
- * @param {string} section - This string represents a task-section.
+ * @param {number} i - This represents the number of all tasks.
  */
+function sortEliminateTasks(i) {
+    if (allTasks[i].section == 'eliminate') {
+        eliminateTasks.push(allTasks[i]);
+        let subTasks = eliminateTasks;
+        insertTasks(subTasks);
+    }
+}
 
-function insertEliminateTasks(section) {
-    for (i = 0; i < eliminateTasks.length; i++) {
-        const task = eliminateTasks[i];
-        section = 'eliminate';
-        document.getElementById('eliminate').innerHTML +=
-            generateTask(task, section);
+
+/**
+ * This function inserts the tasks from the subsections into the subsection-fields in the matrix.
+ * 
+ * 
+ * @param {string} subTasks - This string represents a task-section.
+ */
+function insertTasks(subTasks) {
+    for (i = 0; i < subTasks.length; i++) {
+        let task = subTasks[i];
+        document.getElementById(task.section).innerHTML +=
+            generateTask(task);
     }
 }
 
@@ -106,9 +95,9 @@ function insertEliminateTasks(section) {
  * @param {string} task - This is the task that is put into the matrix.
  * @param {string} section - This string represents a task-section.
  */
-function generateTask(task, section) {
+function generateTask(task) {
     return `
-    <div class="task-card ${section}">
+    <div class="task-card ${task.section}">
     <div class="task-card-left">
         <div>
             <div class="date">${task['date']}</div>
