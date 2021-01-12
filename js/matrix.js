2413326a89ef65a6e3a3559c790b4f3e10ee0066
Dan Mercurean <danymercurean@yahoo.com>
2021-01-12 20:18:56 +0100

    added drag'n drop to the matrix

--- js/matrix.js
+++ js/matrix.js
@@ -123,7 +123,7 @@ function insertTasks(subTasks) {
  */
 function generateTask(task) {
     return `
-    <div class="task-card ${task.section}" draggable="true" ondragstart="dragTask(event)">
+    <div class="task-card ${task.section}" id="drag" draggable="true" ondragstart="drag(event)">
         <div class="task-card-top">
             <div class="date">${task['date']}</div> 
             <img class="delete-icon" src="./img/delete.png" onclick="openDeleteWindow(${task['id']})">
@@ -200,42 +200,33 @@ function deleteTask(taskId) {
 
 /**
  * This method allows to drop an element over an area
- * @param {DataTransfer} ev - The event created from an HTML5 drop down event
+ * @param {DataTransfer} ev 
  */
 function allowDrop(ev) {
     ev.preventDefault();
 }
 
-
 /**
- * This method saves the id of the element that is being dragged
- * @param {DataTransfer} ev - The event created from an HTML5 drop down event
- */
-function dragTask(ev) {
+* This method saves the id of the element that is being dragged
+* @param {DataTransfer} ev 
+*/
+function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
-
 /**
- * This method controls if drop is performed inside the correct area marked with "drop-area"
- * @param {DataTransfer} ev - The event created from an HTML5 drop down event
+ * This method controls if drop is performed inside the correct area
+ * @param {DataTransfer} ev 
  */
-function dropTask(ev) {
-    ev.target.classList.forEach(cssClass => {
-        if (cssClass === 'drop-area') {
-            performDropTask(ev);
-        }
-    });
-}
-
-
-/**
- * This method performs drop of the dropdown 
- * and switches the task to its new place & calls the update function
- * @param {DataTransfer} ev - The event created from an HTML5 drop down event
- */
-function performDropTask(ev) {
+function drop(ev) {
     ev.preventDefault();
     let id = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(id));
 }
+
+//ich würde onclick="dropToScheduleHigh(event)", onclick="dropToScheduleLow(event)" usw machen
+//dann brauchst du nur noch die ID von jedem Element, das du droppst
+function dropToScheduleHigh(event) {
+event
+}
+
