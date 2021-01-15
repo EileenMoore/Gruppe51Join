79e2dd4f2695816ff4311643128adba34dbae3b2
Dan Mercurean <danymercurean@yahoo.com>
2021-01-15 20:19:33 +0100

    drag'n drop update

--- js/matrix.js
+++ js/matrix.js
@@ -2,15 +2,17 @@ let doTasks = [];
 let scheduleTasks = [];
 let delegateTasks = [];
 let eliminateTasks = [];
-
+let draggingTask;
 /**
  * This function sorts the entries of the allTasks array into subsections.
  */
 async function sortTasks() {
     await loadAllTasks();
+    updateHTML();
+}
+function updateHTML() {
     clearSubTasks();
     clearMatrixFields();
-
     for (let i = 0; i < allTasks.length; i++) {
         sortDoTasks(i);
         sortScheduleTasks(i);
@@ -18,7 +20,6 @@ async function sortTasks() {
         sortEliminateTasks(i);
     }
 }
-
 /**
  * This function empties the sub tasks arrays.
  */
@@ -28,7 +29,6 @@ function clearSubTasks() {
     delegateTasks = [];
     eliminateTasks = [];
 }
-
 /**
  * This function empties the tasks in matrix fields.
  */
@@ -38,7 +38,6 @@ function clearMatrixFields() {
     document.getElementById("delegate").innerHTML = "";
     document.getElementById("eliminate").innerHTML = "";
 }
-
 /**
  * This function sorts the tasks with do-section into the doTasks array.
  *
@@ -53,7 +52,6 @@ function sortDoTasks(i) {
         insertTasks(subTasks);
     }
 }
-
 /**
  * This function sorts the tasks with schedule-section into the scheduleTasks array.
  *
@@ -68,7 +66,6 @@ function sortScheduleTasks(i) {
         insertTasks(subTasks);
     }
 }
-
 /**
  * This function sorts the tasks with delegate-section into the delegateTasks array.
  *
@@ -83,7 +80,6 @@ function sortDelegateTasks(i) {
         insertTasks(subTasks);
     }
 }
-
 /**
  * This function sorts the tasks with eliminate-section into the eliminateTasks array.
  *
@@ -98,7 +94,6 @@ function sortEliminateTasks(i) {
         insertTasks(subTasks);
     }
 }
-
 /**
  * This function inserts the tasks from the subsections into the subsection-fields in the matrix.
  *
@@ -111,7 +106,6 @@ function insertTasks(subTasks) {
         document.getElementById(task.section).innerHTML += generateTask(task);
     }
 }
-
 /**
  * This function generates a task card for the matrix
  *
@@ -121,12 +115,11 @@ function insertTasks(subTasks) {
  */
 function generateTask(task) {
     return `
-    <div class="task-card ${task.section}" draggable="true" ondragstart="drag(event)">
+    <div class="task-card ${task.section}" id="drag" draggable="true" ondragstart="drag(${task['id']})">
         <div class="task-card-top">
             <div class="date">${task["date"]}</div> 
-            <img title="delete" class="delete-icon" src="./img/delete.png" onclick="openDeleteWindow(${
-              task["id"]
-            })">
+            <img title="delete" class="delete-icon" src="./img/delete.png" onclick="openDeleteWindow(${task["id"]
+        })">
         </div>
         <div class="task-card-bottom">
             <div class="task-card-left">
@@ -138,7 +131,6 @@ function generateTask(task) {
          </div>
     </div>`;
 }
-
 /**
  * This function generates an image-row of the profile pictures that are involved in the task.
  *
@@ -147,15 +139,12 @@ function generateTask(task) {
  */
 function generateImageRow(task) {
     let imgRow = `<div class="profile-pictures">`;
-
     for (j = 0; j < task["assignedPeople"].length; j++) {
         imgRow += `<img class="profile-picture-small" src="${task["assignedPeople"][j]["profilePicture"]}">`;
     }
-
     imgRow += `</div>`;
     return imgRow;
 }
-
 /**
  * This function is used to open the delete window.
  *
@@ -167,7 +156,6 @@ function openDeleteWindow(taskId) {
         .getElementById("delete-container-overlay")
         .classList.remove("d-none");
     document.getElementById("delete-container").classList.remove("d-none");
-
     document.getElementById("delete-container").innerHTML = `
     <div class="delete-window">
         <span>Do you really want to delete this task?</span>
@@ -177,7 +165,6 @@ function openDeleteWindow(taskId) {
             </div>
     </div>`;
 }
-
 /**
  * This function is used to close the delete window.
  */
@@ -185,7 +172,6 @@ function closeDeleteWindow() {
     document.getElementById("delete-container-overlay").classList.add("d-none");
     document.getElementById("delete-container").classList.add("d-none");
 }
-
 /**
  * This function deletes a task.
  *
@@ -198,39 +184,25 @@ function deleteTask(taskId) {
     closeDeleteWindow();
     sortTasks();
 }
-
-
-/* 
 /**
  * This method allows to drop an element over an area
  * @param {DataTransfer} ev
  */
-/* function allowDrop(ev) {
+function allowDrop(ev) {
     ev.preventDefault();
-} */
-
+}
 /**
-<<<<<<< HEAD
-* This method saves the id of the element that is being dragged
-* @param {DataTransfer} ev 
-*/
-/* function drag(ev) {
-=======
  * This method saves the id of the element that is being dragged
  * @param {DataTransfer} ev
  */
-function drag(ev) {
->>>>>>> d3127bd728e6a93c33355f3dcca534ee642af1e2
-    ev.dataTransfer.setData("text", ev.target.id);
-} */
-
+function drag(id) {
+    draggingTask = id;
+    // ev.dataTransfer.setData("text", ev.target.id);
+}
 /**
  * This method controls if drop is performed inside the correct area marked with "drop-area"
  * @param {DataTransfer} ev
  */
-<<<<<<< HEAD
-/* function drop(ev) {
-=======
 function drop(ev) {
     ev.target.classList.forEach((cssClass) => {
         if (cssClass === "drop-area") {
@@ -238,29 +210,20 @@ function drop(ev) {
         }
     });
 }
-
+function moveTo(section) {
+    let currentTask = allTasks.find(t => t['id'] == draggingTask); // Findet aktuellen Task
+    currentTask.section = section;
+    console.log('This task should be moved to "Schedule"', currentTask);
+    updateHTML();
+    // TODO: Update on Server
+}
 /**
  * This method performs drop of the dropdown
  * and switches the task to its new place & calls the update function
  * @param {DataTransfer} ev 
  */
 function performDropTask(ev) {
->>>>>>> d3127bd728e6a93c33355f3dcca534ee642af1e2
     ev.preventDefault();
     let id = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(id));
-} */
-
-<<<<<<< HEAD
-/* function dropToScheduleHigh(event) {
-event
-} */
-
-
-=======
-//ich würde onclick="dropToScheduleHigh(event)", onclick="dropToScheduleLow(event)" usw machen
-//dann brauchst du nur noch die ID von jedem Element, das du droppst
-function dropToScheduleHigh(event) {
-    event
 }
->>>>>>> d3127bd728e6a93c33355f3dcca534ee642af1e2
